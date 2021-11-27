788f61e618cf8783066845b24de83fb6a3cd7e1b
Aliazbair <ali0995559052@gmail.com>
2021-11-27 19:46:42 +0300

    add

--- pages/index.js
+++ pages/index.js
@@ -12,23 +12,11 @@ export default function Home({ posts }) {
         <title>Dev Blog</title>
       </Head>
 
-      <div className=" grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 mt-12 p-4 overflow-hidden">
+      <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 mt-2 p-4">
         {posts.map((post, index) => (
           <Post key={index} post={post} />
         ))}
-        {/* <article className="prose lg:prose-xl">
-          <h1>Garlic bread with cheese: What the science tells us</h1>
-          <p>
-            For years parents have espoused the health benefits of eating garlic
-            bread with cheese to their children, with the food earning such an
-            iconic status in our culture that kids will often dress up as warm,
-            cheesy loaf for Halloween.
-          </p>
-          <p>
-            But a recent study shows that the celebrated appetizer may be linked
-            to a series of rabies cases springing up around the country.
-          </p>
-        </article> */}
+     
       </div>
     </div>
   );
